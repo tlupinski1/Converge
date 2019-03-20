2a7938dd980e6d99d77941a4ed0b39feba0e2257
jackBonadies <jbonadies6@gmail.com>
2019-03-19 23:51:18 -0400

    fixed default profile bug

--- converge/users/models.py
+++ converge/users/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     #delete user delete images.  also each user gets one profile
-    image = models.ImageField(default='default.jpg',upload_to='pics') #needs pillow to be installed
+    image = models.ImageField(default='/pics/default.png',upload_to='pics') #needs pillow to be installed
     def __str__(self):
         return f'{self.user.username}\'s profile'
