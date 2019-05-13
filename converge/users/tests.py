c71cc4fa6854ce5152ed92a2ab6576faf8048cf5
Gero Alberino <gero.alberino3@gmail.com>
2019-05-13 18:07:11 -0400

    ADD: more tests all pass

--- converge/users/tests.py
+++ converge/users/tests.py
@@ -1,15 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from users.models import Profile, User, Project, Polls, PollAnswers, Members
 from django.utils import timezone
 from .forms import OurUserForm, UpdateUser, UpdateProfile, ProjectForm, textForm, PollsForm, AnswerForm, UpdateUser
+from django.contrib.auth.models import User
+from . import views
+from django.urls import reverse
 
 # Create your tests here.
 class ProjectTests(TestCase):
+    def setUp(self):
+        # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+            username='fake', email='fake@...', password='fake')
 
-    def test_homepage(self):
-        response = self.client.get('/')
-        self.assertEqual(response.status_code, 200)
-
+    #testing forms here
     def test_forms(self):
         form = ProjectForm(data={'projectName': "projectName", 'projectType': "projectType", 'projectDescription': "projectDescription", 'projectPicture': "projectPicture"})
         self.assertTrue(form.is_valid())
@@ -22,7 +27,29 @@ class ProjectTests(TestCase):
         form = UpdateProfile(data={'image': "image", 'userDescription': "userDescription", 'userInterests': "userInterests", 'location': "location"})
         self.assertTrue(form.is_valid())
 
-#Not passing yet
+    #being testing templates here
+    def test_view_register_correct_template(self):
+        response = self.client.get(reverse('register'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/register.html')
+
+    def test_view_allusers_correct_template(self):
+        response = self.client.get(reverse('allusers'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/allUsers.html')
+
+    def test_view_polls_correct_template(self):
+        response = self.client.get(reverse('polls'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/pollsCreate.html')
+
+    def test_view_pollDashboard_correct_template(self):
+        response = self.client.get(reverse('pollDashboard'))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/pollDashboard.html')
+
+
+    #Not passing yet
     #def test_forms_lets_hope(self):
     #    form = AnswerForm(data={'answerOne': "Strongly Disagree", 'answerTwo': "Strongly Disagree", 'answerThree': "Strongly Disagree", 'answerFour': "Strongly Disagree", 'answerFive': "Strongly Disagree"})
     #    self.assertTrue(form.is_valid())
